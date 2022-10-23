661634848680c20a60255e2bf3fd56dca3a9707f
KatarzynaWilczynska <k_wi@o2.pl>
2022-10-23 18:21:28 +0200

    added enterText method

--- src/test/java/pl/tomaszbuga/tests/FirstTest.java
+++ src/test/java/pl/tomaszbuga/tests/FirstTest.java
@@ -2,13 +2,18 @@ package pl.tomaszbuga.tests;
 
 import org.testng.annotations.Test;
 import pl.tomaszbuga.framework.BaseTest;
-import pl.tomaszbuga.pom.HomePage;
+import pl.tomaszbuga.pom.SeleniumTrainingPage;
 
 public class FirstTest extends BaseTest {
     @Test()
     public void firstMethod() {
-        HomePage homePage = new HomePage(getDriver());
-        homePage.openHomePage();
-        homePage.clickSearchButton();
+        SeleniumTrainingPage homePage = new SeleniumTrainingPage(getDriver());
+        homePage.openPage();
+        String testPassword = "testowehaslo";
+        homePage.enterTextToTextInput(testPassword);
+        if(!homePage.getTextFromTextInput().equals(testPassword))
+            org.testng.Assert.fail("Something went wrong, and the password from the input is not equal to the excepted password");
+        if(!homePage.checkIfTypeIsPassword())
+            org.testng.Assert.fail("The 'password' input has the incorrect type attribute");
     }
 }
